16a22fd7a189c723d20e8cc8f833c216986bc447
sandeepkasani <sandeepkasani@gmail.com>
2016-05-14 20:23:59 +0530

    dashboard changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,12 @@
 import {Component} from 'angular2/core';
+import {DashboardComponent} from './dashboard/dashboard.component.ts';
 let appTemplate = require('./app.template.html');
+let styles = require('./header.scss');
 
 @Component({
     selector: 'reviews',
-    template: appTemplate
+    template: appTemplate,
+    styles: ['' + styles],
+    directives: [DashboardComponent]
 })
 export class ReviewsComponent { }
