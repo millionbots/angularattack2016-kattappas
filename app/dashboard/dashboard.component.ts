bcb1c1b90744ce54853cf5f43d82a9de23805fdd
sravikiran <yuvakiran2009@gmail.com>
2016-05-15 23:50:19 +0530

    Made the dashboard slightly dynamic, YEAH!!!

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {ProductsService} from '../products/products.service.ts';
+
 let dashboardTemplate = require('./dashboard.template.html');
 let styles = require('./dashboard.scss');
 
@@ -7,4 +9,33 @@ let styles = require('./dashboard.scss');
     template: dashboardTemplate,
     styles: ['' + styles]
 })
-export class DashboardComponent { }
+export class DashboardComponent { 
+    topProducts: Array<any> = [];
+    
+    constructor(private productsService: ProductsService){
+        
+    }
+    
+    ngOnInit(){
+        this.productsService.getProducts()
+            .subscribe((products) => {
+                
+                let allProds = Array.prototype.concat.apply([],[products.Cameras, products.Mobiles, products.Notebooks]);
+                
+                var sortedProds = allProds.sort(function(prod1, prod2){
+                    if(prod1.rating > prod2.rating)
+                        return -1;
+                    else if(prod1.rating < prod2.rating)
+                        return 1;
+                    else
+                        return 0;
+                });
+                
+                this.topProducts.push(sortedProds[0]);
+                this.topProducts.push(sortedProds[1]);
+                this.topProducts.push(sortedProds[2]);
+                
+                console.log(this.topProducts);
+            });
+    }
+}
