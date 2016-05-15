85e2fb7ef10e43fcfc27d016d1008f0f5a96d830
sravikiran <yuvakiran2009@gmail.com>
2016-05-15 20:03:35 +0530

    Fixed a few merge issues

--- app/products/products.component.ts
+++ app/products/products.component.ts
@@ -1,35 +1,43 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsService} from "./products.service.ts";
 import {Product} from "./products.model.ts";
+import { Router } from '@angular/router-deprecated';
+
 let productsTemplate = require("./products.template.html");
 let styles = require('./product.scss');
 
 @Component({
     selector: 'products',
-    providers: [ProductsService],
     template: productsTemplate,
     styles: ['' + styles]
 })
 export class ProductsComponent implements OnInit {
-    products:any = {};
+    products: any = {};
 
-    constructor(private productsService: ProductsService) {
+    constructor(private productsService: ProductsService,
+               private router: Router) {
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.productsService.getProducts()
             .subscribe(products => {
-              this.products = products
+                console.log(products);
+                this.products = products
             });
     }
 
-    get categories () {
-      return Object.keys(this.products);
+    get categories() {
+        return Object.keys(this.products);
     }
 
-    productSpecs (input: any) {
-      var e = document.createElement('div');
-      e.innerHTML = input;
-      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+    productSpecs(input: any) {
+        var e = document.createElement('div');
+        e.innerHTML = input;
+        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+    }
+    
+    viewDetails(product: any, category: string){
+        let link = ['ProductDetails', { id: product.productId, category: category }];
+        this.router.navigate(link);
     }
 }
